a5fe1b7bacf6910b2242a2fc1cf6ea8cd9c1258f
Enno Rehling <enno.rehling@gmail.com>
2011-09-04 11:24:27 -0700

    unity build for lua
    renaming effort

--- jni/Android.mk
+++ jni/Android.mk
@@ -17,12 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native-activity
-LOCAL_SRC_FILES := main.c \
-	lua/lapi.c lua/lcode.c lua/ldebug.c lua/ldo.c lua/ldump.c lua/lfunc.c lua/lgc.c lua/llex.c lua/lmem.c \
-	lua/lobject.c lua/lopcodes.c lua/lparser.c lua/lstate.c lua/lstring.c lua/ltable.c lua/ltm.c  \
-	lua/lundump.c lua/lvm.c lua/lzio.c \
-	lua/lauxlib.c lua/lbaselib.c lua/ldblib.c lua/liolib.c lua/lmathlib.c lua/loslib.c lua/ltablib.c \
-	lua/lstrlib.c lua/loadlib.c lua/linit.c
+LOCAL_SRC_FILES := main.c lib-luacore.c lib-lualib.c
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_CFLAGS    += -Ijni/lua
