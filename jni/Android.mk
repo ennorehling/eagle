79b490f81784a9b9306f58199843e3cbc4680ab8
Enno Rehling <enno.rehling@gmail.com>
2011-09-14 20:59:53 -0700

    warnings are important, do not ignore them

--- jni/Android.mk
+++ jni/Android.mk
@@ -21,6 +21,7 @@ LOCAL_SRC_FILES := main.c lib-luacore.c lib-lualib.c lib-tolua.c log_lua.c log.c
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_CFLAGS    += -Ijni/lua -Ijni/tolua
+LOCAL_CFLAGS    += -Wall -Werror
 
 include $(BUILD_SHARED_LIBRARY)
 
--- jni/log.c
+++ jni/log.c
@@ -1,4 +1,5 @@
 #include "log.h"
+#include <android/log.h>
 
 void log_write(int prio, const char * tag, const char *text) {
     __android_log_write(prio, tag, text);
