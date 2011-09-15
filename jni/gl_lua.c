ff167e50c031d3113ea040d420f9046830343ba5
Enno Rehling <enno.rehling@gmail.com>
2011-09-14 21:05:30 -0700

    extend the Lua GL module, just a little.

--- jni/gl_lua.c
+++ jni/gl_lua.c
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: gl
-** Generated automatically by tolua++-1.0.92 on Sun Sep  4 21:53:33 2011.
+** Generated automatically by tolua++-1.0.92 on Sun Sep  4 22:04:14 2011.
 */
 
 #ifndef __cplusplus
@@ -55,6 +55,34 @@ static int tolua_gl_gl_glClearColor00(lua_State* tolua_S)
 }
 #endif //#ifndef TOLUA_DISABLE
 
+/* function: glClear */
+#ifndef TOLUA_DISABLE_tolua_gl_gl_glClear00
+static int tolua_gl_gl_glClear00(lua_State* tolua_S)
+{
+#ifndef TOLUA_RELEASE
+ tolua_Error tolua_err;
+ if (
+     !tolua_isnumber(tolua_S,1,0,&tolua_err) ||
+     !tolua_isnoobj(tolua_S,2,&tolua_err)
+ )
+  goto tolua_lerror;
+ else
+#endif
+ {
+  unsigned int tolua_var_5 = ((unsigned int)  tolua_tonumber(tolua_S,1,0));
+  {
+   glClear(tolua_var_5);
+  }
+ }
+ return 0;
+#ifndef TOLUA_RELEASE
+ tolua_lerror:
+ tolua_error(tolua_S,"#ferror in function 'glClear'.",&tolua_err);
+ return 0;
+#endif
+}
+#endif //#ifndef TOLUA_DISABLE
+
 /* Open function */
 TOLUA_API int tolua_gl_open (lua_State* tolua_S)
 {
@@ -64,7 +92,11 @@ TOLUA_API int tolua_gl_open (lua_State* tolua_S)
  tolua_beginmodule(tolua_S,NULL);
   tolua_module(tolua_S,"gl",0);
   tolua_beginmodule(tolua_S,"gl");
+   tolua_constant(tolua_S,"GL_DEPTH_BUFFER_BIT",GL_DEPTH_BUFFER_BIT);
+   tolua_constant(tolua_S,"GL_STENCIL_BUFFER_BIT",GL_STENCIL_BUFFER_BIT);
+   tolua_constant(tolua_S,"GL_COLOR_BUFFER_BIT",GL_COLOR_BUFFER_BIT);
    tolua_function(tolua_S,"glClearColor",tolua_gl_gl_glClearColor00);
+   tolua_function(tolua_S,"glClear",tolua_gl_gl_glClear00);
   tolua_endmodule(tolua_S);
  tolua_endmodule(tolua_S);
  return 1;
